db98f4ea69b87233e4edf5edf5aa701b79087d68
Caique Giovannini <caique_giovannini@hotmail.com>
2022-09-05 23:38:12 -0300

    Add eslint and fix errors

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,41 +1,45 @@
-import { useContext } from "react";
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
-import { dateFormatter, priceFormatter } from "../../utils/formatter";
-import { SearchForm } from "./components/SearchForm";
+import { useContext } from 'react'
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
+import { SearchForm } from './components/SearchForm'
 
-import { PriceHighLight, TransactionContainer, TransactionsTable } from "./styles";
+import {
+  PriceHighLight,
+  TransactionContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
-    const { transactions } = useContext(TransactionsContext)
+  const { transactions } = useContext(TransactionsContext)
 
-    return (
-        <div>
-            <Header />
-            <Summary />
+  return (
+    <div>
+      <Header />
+      <Summary />
 
-            <TransactionContainer>
-                <SearchForm />
+      <TransactionContainer>
+        <SearchForm />
 
-                <TransactionsTable>
-                    <tbody>
-                        {transactions.map(transaction => (
-                            <tr key={transaction.id}>
-                                <td width="50%">{transaction.description}</td>
-                                <td>
-                                    <PriceHighLight variant={transaction.type}>
-                                        {transaction.type === 'outcome' && '- '}
-                                        {priceFormatter.format(transaction.price)}
-                                    </PriceHighLight>
-                                </td>
-                                <td>{transaction.category}</td>
-                                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </TransactionsTable>
-            </TransactionContainer>
-        </div>
-    )
+        <TransactionsTable>
+          <tbody>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td width="50%">{transaction.description}</td>
+                <td>
+                  <PriceHighLight variant={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
+                  </PriceHighLight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+              </tr>
+            ))}
+          </tbody>
+        </TransactionsTable>
+      </TransactionContainer>
+    </div>
+  )
 }
